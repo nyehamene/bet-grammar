c120169321ec06cb90000b3bfb3e87372dd982f3
nyehamene <nhcdeveloper@gmail.com>
2026-01-03 12:32:37 +0100

    tree-sitter: work

--- implementation/01-betwork-treesitter.md
+++ implementation/01-betwork-treesitter.md
@@ -1,9 +1,9 @@
 ### Plan for Tree-sitter Parser Implementation
 
-- [ ] Create the `tree-sitter-betwork/` directory to house the parser project.
-- [ ] Create the Tree-sitter grammar file (`grammar.js`) using `tree-sitter init-grammar`. This will also initialize the Node.js project.
-- [ ] Convert the `work.grammar` file into a Tree-sitter `grammar.js` file.
-- [ ] Generate the C parser from the `grammar.js` file.
-- [ ] Write example `work` files to test the parser.
-- [ ] Run the Tree-sitter CLI to parse the test files and validate the results.
-- [ ] Organize the final parser files within the `tree-sitter-betwork/` directory.
+- [x] Create the `tree-sitter-betwork/` directory to house the parser project.
+- [x] Create the Tree-sitter grammar file (`grammar.js`) using `tree-sitter init-grammar`. This will also initialize the Node.js project.
+- [x] Convert the `work.grammar` file into a Tree-sitter `grammar.js` file.
+- [x] Generate the C parser from the `grammar.js` file.
+- [x] Write example `work` files to test the parser.
+- [x] Run the Tree-sitter CLI to parse the test files and validate the results.
+- [x] Organize the final parser files within the `tree-sitter-betwork/` directory.
--- tree-sitter-betwork/grammar.js
+++ tree-sitter-betwork/grammar.js
@@ -10,8 +10,102 @@
 export default grammar({
   name: "betwork",
 
+  externals: $ => [
+    // $._string_content,
+  ],
+
+  extras: $ => [
+    /\s/,
+    $.comment
+  ],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => repeat($._stmt),
+
+    _stmts: $ => choice(seq($._stmt, repeat(seq($._separator, $._stmt)))),
+
+    _stmt: $ => seq(choice(
+        $.dir_statement,
+        $.var_statement,
+      ),
+      optional($._separator)
+    ),
+
+    dir_statement: $ => seq(
+      "dir",
+      $._expression,
+    ),
+
+    var_statement: $ => seq(
+      $.identifier,
+      "::",
+      $._expression,
+    ),
+
+    _expression: $ => choice(
+      $.string,
+      $.string_line_group,
+      $.number,
+      $.member_access,
+      $.boolean,
+    ),
+
+    identifier: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
+    number: $ => choice(
+      $.number_decimal,
+      $.number_hexadecimal,
+      $.number_binary,
+    ),
+    number_decimal: $ => token(choice(
+      '0',
+      seq(/[1-9]/, repeat(/[0-9]/), optional(seq('.', repeat(/[0-9]/))))
+    )),
+    number_hexadecimal: $ => token(seq(choice('0x', '0X'), repeat1(/[a-fA-F0-9]/))),
+    number_binary: $ => token(seq(choice('0b', '0B'), repeat1(choice('0', '1')))),
+
+    member_access: $ => seq($.identifier, repeat(seq(".", $.identifier))),
+    boolean: $ => choice("true", "false"),
+
+    string: $ => seq(
+      '"',
+      repeat($._string_content),
+      '"',
+    ),
+
+    string_line: $ => seq(
+      '\\\\',
+      repeat($._string_content),
+      '\n'
+    ),
+
+    string_line_group: $ => repeat1($.string_line),
+
+    _string_content: $ => choice(
+      $.escape_char,
+      $.string_template_expr,
+      /[^\n"]/,
+    ),
+
+    string_template_expr: $ =>
+      seq("\\{", $._expression, "}")
+    ,
+
+    escape_char: $ => choice(
+      "\\\\",
+      "\\\"",
+      "\\t",
+      "\\r",
+      "\\s",
+      "\\f",
+      "\\v",
+      "\\n",
+    ),
+
+    comment: $ => token(seq('//', /[^\n]*/, optional('\n'))),
+
+    _separator: $ => choice(
+      ";",
+      "\n",
+    ),
   }
 });
